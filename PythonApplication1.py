6faf4dc836dc0c1fc825ae5bf96e9484719eb328
Rafael Furlan <rafaeel5005@gmail.com>
2021-07-16 04:40:27 -0300

    making the logic better

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -21,14 +21,26 @@ def main():
             source.SAMPLE_WIDTH = 2
             print("speak...")
             audio  = mic.listen(source)
-            try:
-                comando = mic.recognize_google(audio, language="pt-br" or "en-us")
-                print(f"Você disse: {comando}")
-            except sr.UnknownValueError:
-                tts = gTTS(text="Sorry, i couldn't understand..please repeat or try to be more clear", lang='en')
-                tts.save('said1.mp3')
-                playsound('said1.mp3')
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        try:
+            comando = mic.recognize_google(audio, language="pt-br" or "en-us")
+            print(f"Você disse: {comando}")
+        except sr.UnknownValueError:
+            playsound('said1.mp3') #removed TTS save, so i don't get a conflict beetween an already created file (she says: sorry i couldn't understand)
+            main()
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                           
+            
+#space for functions of the assistant:
+
+        if len(comando) > 0:
+            query = comando.lower()
+            print(comando.lower())
+            if "open" and "google" in query:
+                edge_path = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+                webbrowser.register("edge", None, webbrowser.BackgroundBrowser(edge_path))
+                webbrowser.get("edge").open("www.google.com")
+        else:
+            main()
 
 main()
