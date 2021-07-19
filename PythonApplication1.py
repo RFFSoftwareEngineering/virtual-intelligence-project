722c9ec021cd8b9845dc0e90f0689eab120fca5c
Rafael Furlan <rafaeel5005@gmail.com>
2021-07-19 05:14:26 -0300

    very good, pyautogui up and running

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -6,6 +6,8 @@ from os import path
 import time
 import sys
 import webbrowser
+import pyautogui
+from unidecode import unidecode
 
 
 tts = gTTS(text="Hello, i am Ídi, initializing complete..", lang="en")
@@ -42,6 +44,34 @@ def main():
                 edge_path = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
                 webbrowser.register("edge", None, webbrowser.BackgroundBrowser(edge_path))
                 webbrowser.get("edge").open("www.google.com")
+                tts1 = gTTS(text="what you want me to search for? Mister Creator", lang="en")
+                if os.path.isfile("said3.mp3") == True:
+                    playsound("said3.mp3")
+                else:
+                    tts1.save("said3.mp3")
+                    playsound("said3.mp3")
+                mic = sr.Recognizer()
+                mic.pause_threshold = 3.4
+                with sr.Microphone() as source:
+                    mic.adjust_for_ambient_noise(source)
+                    source.SAMPLE_WIDTH = 2
+                    print("speak...")
+                    audio0  = mic.listen(source)
+                try:
+                    comando0 = mic.recognize_google(audio0, language="pt-br" or "en-us")
+                    print(f"Você disse: {comando}")
+                except sr.UnknownValueError:
+                    playsound('said1.mp3')
+                    main()
+                except sr.RequestError as e:
+                    print("Could not request results from Google Speech Recognition service; {0}".format(e)) 
+                if len(comando0) > 0:
+                    subquery = comando0.lower()
+                    print(f"pesquisando: {subquery}")
+                    pyautogui.write(unidecode(subquery), interval=0.05)
+                    pyautogui.press("enter")
+                else:
+                    main()
             elif "fechar desligue pare exit" and "assistente" in query:
                 tts0 = gTTS(text="finishing assistant execution proccess", lang="en")
                 if os.path.isfile("said2.mp3") == True:
